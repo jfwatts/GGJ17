2959ff6f33aebae4926de1082d90e38e4ae3c425
tyguy <tyler.g.a.smith@gmail.com>
2017-01-21 14:54:14 -0800

    working chain cutter

---
+++ Assets/Scripts/IgnoreCollisionTarget.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollisionTarget : MonoBehaviour {
+	public Collider target;
+	// Use this for initialization
+	void Start () {
+		Physics.IgnoreCollision (GetComponent<Collider> (), target, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
