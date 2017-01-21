a9e9733ae907808ca51baba7f1336ef0eb1e1d25
tyguy <tyler.g.a.smith@gmail.com>
2017-01-21 10:16:23 -0800

    added mug and mug shatter

---
+++ Assets/Scripts/LifeSpan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeSpan : MonoBehaviour {
+	public float lifeSpan = 2.0f;
+	// Use this for initialization
+	void Start () {
+		Invoke ("Die", lifeSpan);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Die(){
+		Destroy (gameObject);
+	}
+}
