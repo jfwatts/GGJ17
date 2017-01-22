f653bb5b1e3d955d6b72fef456a81b956eac496e
tyguy <tyler.g.a.smith@gmail.com>
2017-01-22 02:06:37 -0800

    stuff

---
+++ Assets/Scripts/DoorSlam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSlam : MonoBehaviour {
+	private AudioSource mySound;
+	public GameObject door;
+	// Use this for initialization
+	void Start () {
+		mySound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			mySound.Play ();
+			door.transform.position = Vector3.zero;
+			door.transform.eulerAngles = Vector3.zero;
+			door.GetComponent<Rigidbody>().isKinematic = true;;
+		}
+	}
+}
